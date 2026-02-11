e18d63ac5bdc2a34dd3ddc38bbcf8cee668ac1dd
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-10 23:08:24 -0600

    Bump version: 0.1.3 → 0.1.4

--- documentdb_mcp/documentdb_mcp.py
+++ documentdb_mcp/documentdb_mcp.py
@@ -27,7 +27,7 @@ from fastmcp.utilities.logging import get_logger
 from documentdb_mcp.utils import to_boolean, to_integer
 from documentdb_mcp.middlewares import UserTokenMiddleware, JWTClaimsLoggingMiddleware
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
