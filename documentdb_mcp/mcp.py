94fbf29f38f87adfc09a589ebdd790cb226bf1b6
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-27 01:46:19 -0600

    Bump version: 0.1.17 → 0.1.18

--- documentdb_mcp/mcp.py
+++ documentdb_mcp/mcp.py
@@ -32,7 +32,7 @@ from agent_utilities.middlewares import (
     JWTClaimsLoggingMiddleware,
 )
 
-__version__ = "0.1.17"
+__version__ = "0.1.18"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
