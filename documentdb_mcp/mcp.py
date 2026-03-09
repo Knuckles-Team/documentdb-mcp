505041f2d17765da22e3f6e8b7007d986f6fa873
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-08 21:31:17 -0500

    Bump version: 0.1.32 → 0.1.33

--- documentdb_mcp/mcp.py
+++ documentdb_mcp/mcp.py
@@ -21,7 +21,7 @@ from agent_utilities.mcp_utilities import (
     config,
 )
 
-__version__ = "0.1.32"
+__version__ = "0.1.33"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
